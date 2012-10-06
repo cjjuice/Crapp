b5687f19cd5506db05da08b2a19d590b6ff06046
= <=>
2012-10-05 23:49:19 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrappyApi::Application.config.secret_token = '4f32c49783f8e8e43e6f456e789eb7284c4878e3a7c605bc9e5aa8448c54b7a2484699393eb97879a61c4dcb6beb6b35114096c36c0669586a673ff8fe11967e'
