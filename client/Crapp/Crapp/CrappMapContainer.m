dbaa63781144a23b00c97642e94f803cc8081f7e
deepak vokaliga <deepak.vokaliga@gmail.com>
2012-10-07 09:44:14 -0400

    map update

--- client/Crapp/Crapp/CrappMapContainer.m
+++ client/Crapp/Crapp/CrappMapContainer.m
@@ -24,8 +24,8 @@
         [webview loadHTMLString:fileText baseURL:nil];
         
         NSString *jsCommand = [NSString stringWithFormat:@"addMarker(%f, %f)",-33.890542, 151.274856];
-       
         [webview stringByEvaluatingJavaScriptFromString:jsCommand];
+        
         [webview release];
         
         
